7b4e8692cd09e794c0ddbfcbc303dfdc08158bc7
Kevin Suñer <ksuner@pm.me>
2024-07-14 19:05:31 +0200

    feat: add required constants and variables

--- main.go
+++ main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
-	"log/slog"
+	"fmt"
 	"os"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/viper"
 )
 
-var stepper = newStepper()
+const (
+	subjectLength = 50
+	lineLength    = 72
+)
+
+var (
+	subject strings.Builder
+	stepper = newStepper()
+)
 
 func main() {
 	viper.SetConfigName("zenimmit")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 	if err := viper.ReadInConfig(); err != nil {
-		slog.Error("failed to read config", "error", err)
+		fmt.Printf("failed to read config file: %s\n", err.Error())
 		os.Exit(1)
 	}
 
 	program := tea.NewProgram(newList(viper.GetStringSlice("types")))
 	if _, err := program.Run(); err != nil {
-		slog.Error("failed to run program", "error", err)
+		fmt.Printf("failed to run program: %s\n", err.Error())
 		os.Exit(1)
 	}
 }
